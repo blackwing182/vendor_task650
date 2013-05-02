2d26ece27d22ef24e3947441082fa74e1cf736d7
task650 <task650@gmail.com>
2013-05-02 14:17:19 -0700

    Add jflteatt to build
    Signed-off-by: task650 <task650@gmail.com>
    
    Fix some shitz
    
    Signed-off-by: task650 <task650@gmail.com>

---
+++ products/jflteatt.mk
@@ -0,0 +1,23 @@
+$(call inherit-product, device/samsung/jfltetmo/full_jflteatt.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/aokp/configs/nfc_enhanced.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/aokp/configs/common.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := aokp_jflteatt
+PRODUCT_BRAND := Samsung
+PRODUCT_DEVICE := jflteatt
+PRODUCT_MODEL := SGH-I337
+PRODUCT_MANUFACTURER := Samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteatt TARGET_DEVICE=jflteatt BUILD_FINGERPRINT="samsung/jflteuc/jflteatt:4.2.2/JDQ39/I337UCUAMDB:user/release-keys" PRIVATE_BUILD_DESC="jflteuc-user 4.2.2 JDQ39 I337UCUAMDB release-keys"
+
+# boot animation
+PRODUCT_COPY_FILES += \
+    vendor/aokp/prebuilt/bootanimation/bootanimation_720_1280.zip:system/media/bootanimation.zip
