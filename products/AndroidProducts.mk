0287f7c47f400c8d7c9217517c763fb8c21698bb
task650 <task650@gmail.com>
2013-05-29 17:42:52 -0700

    Add jfltetmo
    Signed-off-by: task650 <task650@gmail.com>

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -4,6 +4,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/d2tmo.mk \
     $(LOCAL_DIR)/d2vzw.mk \
     $(LOCAL_DIR)/jflteatt.mk \
+    $(LOCAL_DIR)/jfltetmo.mk \
     $(LOCAL_DIR)/manta.mk \
     $(LOCAL_DIR)/p4wifi.mk \
     $(LOCAL_DIR)/i777.mk 
