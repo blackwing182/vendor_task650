5a91d24b7c57f124108b8a784eb274917feaaea6
task650 <task650@gmail.com>
2013-09-11 04:44:55 -0700

    Clean up some junk

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,11 +1,6 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/d2att.mk \
-    $(LOCAL_DIR)/d2spr.mk \
-    $(LOCAL_DIR)/d2tmo.mk \
-    $(LOCAL_DIR)/d2vzw.mk \
     $(LOCAL_DIR)/jflteatt.mk \
-    $(LOCAL_DIR)/jfltetmo.mk \
     $(LOCAL_DIR)/manta.mk \
-    $(LOCAL_DIR)/p4wifi.mk \
     $(LOCAL_DIR)/i777.mk 
     
