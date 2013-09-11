968ab146d46916e1a728860a162e3d5e0ed36f75
task650 <task650@gmail.com>
2013-09-10 19:40:07 -0700

    Update jflteatt fingerprint

--- products/jflteatt.mk
+++ products/jflteatt.mk
@@ -13,7 +13,7 @@ PRODUCT_DEVICE := jflteatt
 PRODUCT_MODEL := SGH-I337
 PRODUCT_MANUFACTURER := Samsung
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteatt TARGET_DEVICE=jflteatt BUILD_FINGERPRINT="samsung/jflteuc/jflteatt:4.2.2/JDQ39/I337UCUAMDB:user/release-keys" PRIVATE_BUILD_DESC="jflteuc-user 4.2.2 JDQ39 I337UCUAMDB release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jflteatt TARGET_DEVICE=jflteatt BUILD_FINGERPRINT="samsung/jflteuc/jflteatt:4.2.2/JDQ39/I337UCUAMDL:user/release-keys" PRIVATE_BUILD_DESC="jflteuc-user 4.2.2 JDQ39 I337UCUAMDL release-keys"
 
 # boot animation
 PRODUCT_COPY_FILES += \
